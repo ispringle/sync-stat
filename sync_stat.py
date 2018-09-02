224b40f34dee49474ada5c8d385e09b5506e7985
Ian S Pringle <ipringle@protonmail.com>
2018-09-02 17:05:14 -0400

    Classified all the things

--- sync_stat.py
+++ sync_stat.py
@@ -2,76 +2,83 @@
 
 import pickle
 from slackclient import SlackClient
-inport sys
+import sys
 
-if __name__ == "__main__":
-	main(sys.argv)
+class Post(object):
+	
+	def __init__(self, status, workspaces):
+		syncTo = self.select_workspaces(workspaces)
+		self.update_status(status, syncTo)
+	
+	def update_status(self, status, workspaces):
+		for space in workspaces:
+			sc = SlackClient(space['auth'])
+			sc.api_call(
+				"users.profile.set",
+				profile = status
+				)
+	
+	def select_workspaces(self, work):
+		syncTo = []
+		for workspace in work.spaces:
+			selection = input("Sync status to %s? [y/n]" % workspace)
+			if selection == "y" or selection == "Y" or selection == "yes":
+				syncTo.append(work.spaces[workspace])
+		return syncTo
 
+class Status(object):
+	
+	def set():
+		status = input("Status: ")
+		emoji  = input("Emoji: ")
+		return { "status_text" : status, "status_emoji" : emoji }
 
-def update_status(status, workspaces):
-	for space in workspaces:
-		sc = SlackClient(space['auth'])
-		sc.api_call(
-			"users.profile.set",
-			profile = status
-			)
-
-def set_status():
-	status = input("Status: ")
-	emoji  = input("Emoji: ")
-	return { "status_text" : status, "status_emoji" : emoji }
-
-def select_workspaces():
-	syncTo = []
-	for workspace in Spaces.spaces:
-		selection = input("Sync status to %s? [y/n]" % workspace)
-		if selection == "y" or selection == "Y" or selection == "yes":
-			syncTo.append(Spaces.spaces[workspace])
-	return syncTo
-
-class Spaces(self):
+class Spaces(object):
 	
 	def __init__(self):
 		try:
-			self.spaces = pickle.load( open('spaces.p', 'rb'))
+			self.spaces = pickle.load(open('spaces.p', 'rb'))
 		except:
-			add_workspace()
-			self.spaces = pickle.load( open('spaces.p', 'rb'))
+			makeNew = input("No workspaces found. Would you like to setup a workspace?[y/n]: ")
+			if makeNew == 'y' or makeNew == 'Y' or makeNew == 'yes' or makeNew == 'Yes' or makeNew == 'YES':
+				self.add_workspace()
+				spaces = pickle.load(open('spaces.p', 'rb'))
+			else:
+				quit()
 	
-def add_workspace():
-	spaces = {}
-	add = True
-	while add:
-		workspace = input("Workspace nickname: ")
-		client = input("What chat client is this for? [Slack/Discord]: ")
-		token = input("Workspace legacy token or App token: ")
-		spaces[workspace] = { "auth" : token, "client" : client }
-		next = input("Add another workspace?: [y/n]")
-		if next == 'y' or next == "Y" or next == "yes":
-			pass
-		else:
-			add = False
-	pickle.dump(spaces, open("spaces.p", "wb"))
+	def add_workspace(self):
+		spaces = {}
+		add = True
+		while add:
+			workspace = input("Workspace nickname: ")
+			#client = input("What chat client is this for? [Slack/Discord]: ")
+			client = "Slack"
+			token = input("Workspace legacy token or App token: ")
+			spaces[workspace] = { "auth" : token, "client" : client }
+			next = input("Add another workspace?: [y/n]")
+			if next == 'y' or next == "Y" or next == "yes":
+				pass
+			else:
+				add = False
+		pickle.dump(spaces, open("spaces.p", "wb"))
 
-status = set_status()
-syncTo = select_workspaces()
-print("Posting status")
-update_status(status, syncTo)
-
-def main():
+def main(argv):
+	#Load workspaces if they exist, query to add workspaces if they do not
+	work = Spaces()
+	
+	#Check argv for input
+	#Needs to be replaced with an argv parser
 	try:
-		pickle.load( open('spaces.p', 'rb'))
-		spaces = Spaces():
+		script, option = argv
+		if option == "add":
+			Spaces.add_workspace()
 	except:
-		makeNew = input("No workspaces found. Would you like to setup a workspace?[y/n]: ")
-		if makeNew = 'y' or makeNew = 'Y' or makeNew = 'yes' or makeNew = 'Yes' or makeNew = 'YES':
-			spaces = Spaces()
-		else:
-			quit()
-#try:
-#	script, option = argv
-#	if option == "add":
-#		add_workspace()
-#except:
-#	pass
+		pass
+
+	#Get status, select workspaces to sync to, and post to selected workspaces
+	status = Status.set()
+	Post(status, work)
+
+if __name__ == "__main__":
+	main(sys.argv)
 
